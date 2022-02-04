e091024adb7edd9b1d8bcc59b2adc9d450bb4893
Sara Perkaj <saraperkaj@pursuit.org>
2022-02-03 22:02:56 -0500

    trying something out for netlify to work.

--- app.js
+++ app.js
@@ -5,11 +5,12 @@ const cors = require("cors");
 const transactions = require("./controllers/transactions");
 app.use(cors());
 app.use(express.json());
-app.use("/transactions", transactions);
 
 app.get("/", (request, respond) => {
   respond.status(200).send("Welcome to the Budgeting App");
 });
+
+app.use("/transactions", transactions);
 
 app.get("*", (request, respond) => {
   respond.status(404).json({ Error: "Page not found!" });
